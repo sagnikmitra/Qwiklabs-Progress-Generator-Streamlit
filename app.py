82851db8700bb76f8ed52924719e3f5cc206d31c
sagnikmitra <sagnikmitra123@gmail.com>
2021-04-09 02:40:07 +0530

    type

--- app.py
+++ app.py
@@ -22,89 +22,124 @@ st.write("""
 # Check your Qwiklabs Progress
 """)
 flag = 0
-str = st.text_input('Enter You Qwiklabs Email Id')
-for i in range(2, sheet.max_row+1):
-    if sheet.cell(row=i, column=2).value == str:
-        st.write(sheet.cell(row=i, column=5).value)
-        st.write(sheet.cell(row=i, column=6).value)
-        st.write(int(sheet.cell(row=i, column=7).value))
-        st.write(int(sheet.cell(row=i, column=8).value))
-        flag = flag + 1
 
-if flag==0 :
-    st.write("No Search Found")
+selected = st.sidebar.selectbox("Search By",("Name","Email","Public Profile URL"))
+if selected == "Email": 
+    str = st.text_input('Enter You Qwiklabs Email Id')
+    for i in range(2, sheet.max_row+1):
+        if sheet.cell(row=i, column=2).value == str:
+            st.write(sheet.cell(row=i, column=1).value)
+            st.write(sheet.cell(row=i, column=2).value)
+            st.write(sheet.cell(row=i, column=5).value)
+            st.write(sheet.cell(row=i, column=6).value)
+            st.write(int(sheet.cell(row=i, column=7).value))
+            st.write(int(sheet.cell(row=i, column=8).value))
+            flag = flag + 1
+
+    if flag==0 :
+        st.write("No Search Found")
+
+if selected == "Name": 
+    str = st.text_input('Enter You Qwiklabs Name')
+    for i in range(2, sheet.max_row+1):
+        if sheet.cell(row=i, column=1).value == str:
+            st.write(sheet.cell(row=i, column=1).value)
+            st.write(sheet.cell(row=i, column=2).value)
+            st.write(sheet.cell(row=i, column=5).value)
+            st.write(sheet.cell(row=i, column=6).value)
+            st.write(int(sheet.cell(row=i, column=7).value))
+            st.write(int(sheet.cell(row=i, column=8).value))
+            flag = flag + 1
+
+    if flag==0 :
+        st.write("No Search Found")
+
+if selected == "Public Profile URL": 
+    str = st.text_input('Enter You Qwiklabs PublicUrl')
+    for i in range(2, sheet.max_row+1):
+        if sheet.cell(row=i, column=6).value == str:
+            st.write(sheet.cell(row=i, column=1).value)
+            st.write(sheet.cell(row=i, column=2).value)
+            st.write(sheet.cell(row=i, column=5).value)
+            st.write(sheet.cell(row=i, column=6).value)
+            st.write(int(sheet.cell(row=i, column=7).value))
+            st.write(int(sheet.cell(row=i, column=8).value))
+            flag = flag + 1
+
+    if flag==0 :
+        st.write("No Search Found")
 
 
-dataset_name = st.sidebar.selectbox("Select Dataset",("Iris","Breast Cancer","Wine Dataset"))
-st.write(f"## Name of the Dataset: {dataset_name}")
-classifier_name = st.sidebar.selectbox("Select Classifier",("KNN","SVM","Random Forest"))
+# dataset_name = st.sidebar.selectbox("Select Dataset",("Iris","Breast Cancer","Wine Dataset"))
+# st.write(f"## Name of the Dataset: {dataset_name}")
 
-# def get_quests:
-#     pass
 
-def get_dataset(dataset_name):
-    if dataset_name == "Iris":
-        data = datasets.load_iris()
-    elif dataset_name == "Breast Cancer":
-        data = datasets.load_breast_cancer()
-    else:
-        data = datasets.load_wine()
-    X = data.data
-    y = data.target
-    return X,y
+# # def get_quests:
+# #     pass
 
-X, y = get_dataset(dataset_name)
-st.write("## Shape of Dataset:",X.shape)
-st.write("## Number of classes:",len(np.unique(y)))
+# def get_dataset(dataset_name):
+#     if dataset_name == "Iris":
+#         data = datasets.load_iris()
+#     elif dataset_name == "Breast Cancer":
+#         data = datasets.load_breast_cancer()
+#     else:
+#         data = datasets.load_wine()
+#     X = data.data
+#     y = data.target
+#     return X,y
 
-def add_parameter_ui(classifier_name):
-    params = dict()
-    if classifier_name == "KNN":
-        K = st.sidebar.slider("K",1,15)
-        params["K"] = K
-    elif classifier_name == "SVM":
-        C = st.sidebar.slider("C",0.01,10.0)
-        params["C"] = C
-    elif classifier_name == "Random Forest":
-        max_depth = st.sidebar.slider("max_depth",2,15)
-        n_estimators = st.sidebar.slider("n_estimators",1,100)
-        params["max_depth"] = max_depth
-        params["n_estimators"] = n_estimators
-    return params
+# X, y = get_dataset(dataset_name)
+# st.write("## Shape of Dataset:",X.shape)
+# st.write("## Number of classes:",len(np.unique(y)))
 
-params = add_parameter_ui(classifier_name)
+# def add_parameter_ui(classifier_name):
+#     params = dict()
+#     if classifier_name == "KNN":
+#         K = st.sidebar.slider("K",1,15)
+#         params["K"] = K
+#     elif classifier_name == "SVM":
+#         C = st.sidebar.slider("C",0.01,10.0)
+#         params["C"] = C
+#     elif classifier_name == "Random Forest":
+#         max_depth = st.sidebar.slider("max_depth",2,15)
+#         n_estimators = st.sidebar.slider("n_estimators",1,100)
+#         params["max_depth"] = max_depth
+#         params["n_estimators"] = n_estimators
+#     return params
 
-def get_classifier(classifier_name, params):
-    if classifier_name == "KNN":
-        classifier = KNeighborsClassifier(n_neighbors=params["K"])
-    elif classifier_name == "SVM":
-        classifier = SVC(C=params["C"])
-    elif classifier_name == "Random Forest":
-        classifier = RandomForestClassifier(n_estimators=params["n_estimators"],max_depth=params["max_depth"],random_state=1234)
-    return classifier
+# params = add_parameter_ui(classifier_name)
 
-classifier = get_classifier(classifier_name,params)
+# def get_classifier(classifier_name, params):
+#     if classifier_name == "KNN":
+#         classifier = KNeighborsClassifier(n_neighbors=params["K"])
+#     elif classifier_name == "SVM":
+#         classifier = SVC(C=params["C"])
+#     elif classifier_name == "Random Forest":
+#         classifier = RandomForestClassifier(n_estimators=params["n_estimators"],max_depth=params["max_depth"],random_state=1234)
+#     return classifier
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 1234)
+# classifier = get_classifier(classifier_name,params)
 
-classifier.fit(X_train,y_train)
-y_pred = classifier.predict(X_test)
+# X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 1234)
 
-accuracy_score = accuracy_score(y_test, y_pred)
+# classifier.fit(X_train,y_train)
+# y_pred = classifier.predict(X_test)
 
-st.write(f"## Classifier = {classifier_name}")
-st.write(f"## Accuracy = {accuracy_score}")
+# accuracy_score = accuracy_score(y_test, y_pred)
 
-pca = PCA(2)
-X_projected = pca.fit_transform(X)
+# st.write(f"## Classifier = {classifier_name}")
+# st.write(f"## Accuracy = {accuracy_score}")
 
-x1 = X_projected[:,0]
-x2 = X_projected[:,1]
+# pca = PCA(2)
+# X_projected = pca.fit_transform(X)
 
-fig = plt.figure()
-plt.scatter(x1,x2,c=y,alpha=0.8,cmap = "viridis")
-plt.xlabel("Principal Component 1")
-plt.ylabel("Principal Component 2")
-plt.colorbar()
+# x1 = X_projected[:,0]
+# x2 = X_projected[:,1]
 
-st.pyplot(fig)
+# fig = plt.figure()
+# plt.scatter(x1,x2,c=y,alpha=0.8,cmap = "viridis")
+# plt.xlabel("Principal Component 1")
+# plt.ylabel("Principal Component 2")
+# plt.colorbar()
+
+# st.pyplot(fig)
